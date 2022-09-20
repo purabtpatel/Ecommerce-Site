b7f33151126c8743847db26b3d47623b5e9f68e2
ptp25 <ptp25@njit.edu>
2022-09-20 18:12:06 -0400

    Update problem1.php

--- public_html/M2/problem1.php
+++ public_html/M2/problem1.php
@@ -5,10 +5,12 @@ $a3 = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
 $a4 = [0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10];
 function processArray($arr) {
     //use the $arr variable to iterate over
-    echo "<br>Processing Array:<br><pre>" . var_export($arr, $arr%2 == 1) . "</pre>";
+    echo "<br>Processing Array:<br><pre>" . var_export($arr, true) . "</pre>";
     echo "<br>Odds output:<br>";
     //TODO add logic here to echo out only odd values
-
+    if($arr % 2==1){
+        echo $arr;
+    }
 }
 echo "Problem 1: Odd Output<br>";
 ?>
