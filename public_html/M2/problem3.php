5d69d0aad17954c0018038d0406e680c9eb72967
ptp25 <ptp25@njit.edu>
2022-09-22 11:59:43 -0400

    Update Problem3.php

--- public_html/M2/problem3.php
+++ public_html/M2/problem3.php
@@ -10,6 +10,7 @@ function bePositive($arr) {
     //TODO use echo to output all of the values as positive (even if they were originally positive)
     foreach($arr as $temp){
         echo(abs($temp));
+        echo("<br>");
     }
 }
 echo "Problem 3: Be Positive<br>";
