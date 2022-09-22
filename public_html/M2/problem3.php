32b4cdf383d828fe708fc60e2d4dee032ba20ef5
ptp25 <ptp25@njit.edu>
2022-09-22 17:13:50 -0400

    Added M2-Submission.md

--- public_html/M2/problem3.php
+++ public_html/M2/problem3.php
@@ -8,9 +8,13 @@ function bePositive($arr) {
     echo "<br>Processing Array:<br><pre>" . var_export($arr, true) . "</pre>";
     echo "<br>Positive output:<br>";
     //TODO use echo to output all of the values as positive (even if they were originally positive)
+    /*
+    ptp25 - 09/22/22
+    abs outputs the absolute value
+    of the integer or float given
+    */
     foreach($arr as $temp){
         echo(abs($temp) . "<br>");
-        
     }
 }
 echo "Problem 3: Be Positive<br>";
