c99e41154f672fe8d8c69bef6ff0c07511d8a6fe
purabtpatel <purabtpatel24@gmail.com>
2022-10-14 18:55:43 -0400

    update login.php

--- public_html/Project/login.php
+++ public_html/Project/login.php
@@ -60,6 +60,7 @@ if (isset($_POST["email"]) && isset($_POST["password"])) {
                     if(password_verify($password, $hash)){
                         echo "Welcome $email";
                         $_SESSION["user"] = $user;
+                        die(header("Location: home.php"));
                     } else {
                         echo "Invalid password";
                     }
