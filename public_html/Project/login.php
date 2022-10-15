a97cbd3d641f7058ef7490a22f0c3b0d0d6cb1bc
purabtpatel <purabtpatel24@gmail.com>
2022-10-15 11:53:07 -0400

    Intro to php Templating changes

--- public_html/Project/login.php
+++ public_html/Project/login.php
@@ -33,9 +33,9 @@ if (isset($_POST["email"]) && isset($_POST["password"])) {
         $hasError = true;
     }
     //sanitize
-    $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+    $email = sanitize_email($email);
     //validate
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    if (!is_valid_email($email)) {
         echo "Invalid email address";
         $hasError = true;
     }
@@ -48,17 +48,18 @@ if (isset($_POST["email"]) && isset($_POST["password"])) {
         $hasError = true;
     }
     if (!$hasError) {
+        //TODO 4
         $db = getDB();
         $stmt = $db->prepare("SELECT email, password from Users where email = :email");
-        try{
+        try {
             $r = $stmt->execute([":email" => $email]);
-            if($r){
+            if ($r) {
                 $user = $stmt->fetch(PDO::FETCH_ASSOC);
-                if($user){
+                if ($user) {
                     $hash = $user["password"];
                     unset($user["password"]);
-                    if(password_verify($password, $hash)){
-                        echo "Welcome $email";
+                    if (password_verify($password, $hash)) {
+                        echo "Weclome $email";
                         $_SESSION["user"] = $user;
                         die(header("Location: home.php"));
                     } else {
@@ -68,12 +69,9 @@ if (isset($_POST["email"]) && isset($_POST["password"])) {
                     echo "Email not found";
                 }
             }
-        } catch(Exception $e) {
+        } catch (Exception $e) {
             echo "<pre>" . var_export($e, true) . "</pre>";
         }
-        //TODO 4
-
-
     }
 }
 ?>
