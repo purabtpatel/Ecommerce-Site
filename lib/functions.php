895fb7b3c2d6cbdc4ff2c083408f4b8b94c2b07a
purabtpatel <purabtpatel24@gmail.com>
2022-11-06 09:29:29 -0500

    Flash Messages

--- lib/functions.php
+++ lib/functions.php
@@ -12,4 +12,22 @@ require(__DIR__ . "/sanitizers.php");
 //TODO 3: User helpers
 require(__DIR__ . "/user_helpers.php");
 //TODO 4: Flash Message Helpers
+function flash($msg = "", $color = "info"){
+    $message = ["text" => $msg, "color" => $color];
+    if(!isset($_SESSION["flash"])){
+        array_push($_SESSION["flash"], $message);
+    } else {
+        $_SESSION["flash"] = array();
+        array_push($_SESSION["flash"], $message);
+    }
+}
+
+function getMessages(){
+    if(isset($_SESSION["flash"])){
+        $flashes = $_SESSION["flash"];
+        $_SESSION["flash"] = array();
+        return $flashes;
+    }
+    return array();
+}
 ?>
