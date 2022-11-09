5bfe2143fed557a1c9588ae9fadc02cf7d70c8b1
Purab Patel <ptp25@njit.edu>
2022-11-09 13:52:00 -0500

    M6 html5 canvas (#33)
    * Created init_db.php
    
    * changed .gitignore to push .sql file
    
    * Init_db.php (#13)
    
    * Init_db.php (#14)
    
    * Feat user registration (#16)
    
    * Init_db.php
    
    * update register.php
    
    * update register and functions
    
    * update file location
    
    * update register.php
    
    * Feat basic nav (#17)
    
    * added login.php, nav.php
    
    * update nav.php
    
    * added home.php
    
    * update login.php
    
    * create logout.php
    
    * Intro to php Templating changes (#18)
    
    * Feat first helper function (#19)
    
    * Intro to php Templating changes
    
    * update M4 submission
    
    * Flash Messages (#22)
    
    * userProfile (#23)
    
    * Added Arcade game.js to M6
    
    * Added all game functionality

--- lib/functions.php
+++ lib/functions.php
@@ -1,11 +1,33 @@
 <?php
 //TODO 1: require db.php
-
+require_once(__DIR__ . "/db.php");
+//This is going to be a helper for redirecting to our base project path since it's nested in another folder
+//This MUST match the folder name exactly
+$BASE_PATH = '/Project/';
 //require safer_echo.php
 require(__DIR__ . "/safer_echo.php");
 //TODO 2: filter helpers
-
+require(__DIR__ . "/sanitizers.php");
 //TODO 3: User helpers
-
+require(__DIR__ . "/user_helpers.php");
 //TODO 4: Flash Message Helpers
+function flash($msg = "", $color = "info"){
+    $message = ["text" => $msg, "color" => $color];
+    if(!isset($_SESSION["flash"])){
+        array_push($_SESSION["flash"], $message);
+    } else {
+        $_SESSION["flash"] = array();
+        array_push($_SESSION["flash"], $message);
+    }
+}
+
+function getMessages(){
+    if(isset($_SESSION["flash"])){
+        $flashes = $_SESSION["flash"];
+        $_SESSION["flash"] = array();
+        return $flashes;
+    }
+    return array();
+}
+
 ?>
---
+++ public_html/Project/home.php
@@ -0,0 +1,15 @@
+<?php
+require(__DIR__ . "/../../partials/nav.php");
+?>
+<h1>Home</h1>
+<?php
+if (is_logged_in()) {
+    flash("Welcome, " . get_user_email());
+} else {
+    flash("You're not logged in");
+}
+//shows session info
+require(__DIR__."/../../partials/flash.php");
+
+
+?>
