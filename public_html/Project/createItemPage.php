f5b308515dee1c5ad8872b65eb6906a70b47117c
purabtpatel <purabtpatel24@gmail.com>
2022-11-22 12:49:23 -0500

    Update createItemPage.php

--- public_html/Project/createItemPage.php
+++ public_html/Project/createItemPage.php
@@ -4,15 +4,31 @@ require(__DIR__ . "/../../partials/nav.php");
 <div class="container-fluid">
     <h1>Login</h1>
     <form onsubmit="return validate(this)" method="POST">
-        <div class="mb-3">
-            <label class="form-label" for="email">Username/Email</label>
-            <input class="form-control" type="text" id="email" name="email" required />
+    <div class="mb-3">
+            <label class="form-label" for="name">Name</label>
+            <input class="form-control" id="name" name="name" required />
         </div>
         <div class="mb-3">
-            <label class="form-label" for="pw">Password</label>
-            <input class="form-control" type="password" id="pw" name="password" required minlength="8" />
+            <label class="form-label" for="d">Description</label>
+            <textarea class="form-control" name="description" id="d"></textarea>
         </div>
-        <input type="submit" class="mt-3 btn btn-primary" value="Login" />
+        <div class="mb-3">
+            <label class="form-label" for="category">Category</label>
+            <input class="form-control" id="category" name="category" required />
+        </div>
+        <div class="mb-3">
+            <label class="form-label" for="price">Price</label>
+            <input class="form-control" id="price" name="price" required />
+        </div>
+        <div class="mb-3">
+            <label class="form-label" for="stock">Stock</label>
+            <input class="form-control" id="stock" name="stock" required />
+        </div>
+        <div class="mb-3">
+            <label class="form-label" for="visibility">Visibility</label>
+            <input class="form-control" id="visibility" name="visibility" required />
+        </div>
+        <input type="submit" class="mt-3 btn btn-primary" value="Submit" />
     </form>
 </div>
 
