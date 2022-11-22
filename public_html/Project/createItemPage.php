705d48a5744ccdb08e771b1849f1d72f27edf459
purabtpatel <purabtpatel24@gmail.com>
2022-11-22 12:51:27 -0500

    Update createItemPage.php

--- public_html/Project/createItemPage.php
+++ public_html/Project/createItemPage.php
@@ -1,8 +1,36 @@
 <?php
-require(__DIR__ . "/../../partials/nav.php");
+require(__DIR__ . "/../../../partials/nav.php");
+if (!has_role("Admin") && !has_role("Shop Owner")) {
+    flash("You don't have permission to view this page", "warning");
+    die(header("Location: " . get_url("home.php")));
+}
+
+if (isset($_POST["save"])) {
+    $name = $_POST["name"];
+    $description = $_POST["description"];
+    $price = $_POST["price"];
+    $stock = $_POST["stock"];
+    $user = get_user_id();
+    $db = getDB();
+    $stmt = $db->prepare("INSERT INTO Products (name, description, price, stock, user_id) VALUES(:name, :description, :price, :stock, :user)");
+    $r = $stmt->execute([
+        ":name" => $name,
+        ":description" => $description,
+        ":price" => $price,
+        ":stock" => $stock,
+        ":user" => $user
+    ]);
+    if ($r) {
+        flash("Created successfully with id: " . $db->lastInsertId());
+    } else {
+        $e = $stmt->errorInfo();
+        flash("Error creating: " . var_export($e, true));
+    }
+}
+
 ?>
 <div class="container-fluid">
-    <h1>Login</h1>
+    <h1>Create New Product</h1>
     <form onsubmit="return validate(this)" method="POST">
     <div class="mb-3">
             <label class="form-label" for="name">Name</label>
