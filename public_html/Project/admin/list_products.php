5e259b649003f0b55ffc4e1a4fedb1390f7730f6
purabtpatel <purabtpatel24@gmail.com>
2022-11-29 15:39:14 -0500

    Update list_products.php

--- public_html/Project/admin/list_products.php
+++ public_html/Project/admin/list_products.php
@@ -25,22 +25,24 @@ $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
                     <p class="card-text">Visibility: <?php safer_echo($r["visibility"]); ?></p>
                 </div>
             </div>
-            <div class="card-footer">
-                <a href="ViewCart.php?id=<?php safer_echo($r["id"]); ?>" class="btn btn-primary">Add to Cart</a>
-            </div>
-            <?php if (has_role("Admin")) : ?>
+            <div>
                 <div class="card-footer">
-                    <!-- pass in all of current products details to edit_products.php-->
-                    <a href="edit_products.php?
-                            id=<?php safer_echo($r['id']); ?>&
-                            name=<?php safer_echo($r['name']) ?>&
-                            description=<?php safer_echo($r['description']) ?>&
-                            category=<?php safer_echo($r['category']) ?>&
-                            unit_price=<?php safer_echo($r['unit_price']) ?>&
-                            stock=<?php safer_echo($r['stock']) ?>&
-                            visibility=<?php safer_echo($r['visibility']) ?>" class="btn btn-primary">Edit</a>
+                    <a href="ViewCart.php?id=<?php safer_echo($r["id"]); ?>" class="btn btn-primary">Add to Cart</a>
                 </div>
-            <?php endif; ?>
+                <?php if (has_role("Admin")) : ?>
+                    <div class="card-footer">
+                        <!-- pass in all of current products details to edit_products.php-->
+                        <a href="edit_products.php?
+                                id=<?php safer_echo($r['id']); ?>&
+                                name=<?php safer_echo($r['name']) ?>&
+                                description=<?php safer_echo($r['description']) ?>&
+                                category=<?php safer_echo($r['category']) ?>&
+                                unit_price=<?php safer_echo($r['unit_price']) ?>&
+                                stock=<?php safer_echo($r['stock']) ?>&
+                                visibility=<?php safer_echo($r['visibility']) ?>" class="btn btn-primary">Edit</a>
+                    </div>
+                <?php endif; ?>
+            </div>
         </div>
     <?php endforeach; ?>
 </div>
