ea658b7fc45c083848c0f1fc259adb2ffd87c079
purabtpatel <purabtpatel24@gmail.com>
2022-11-29 12:15:12 -0500

    edit button

--- public_html/Project/admin/edit_products.php
+++ public_html/Project/admin/edit_products.php
@@ -1,54 +1,69 @@
 <?php
 //form to edit products with input id from list_products.php
 require __DIR__ . "/../../../partials/nav.php";
-if(!has_role("Admin") && !has_role("Shop Owner")){
+if (!has_role("Admin") && !has_role("Shop Owner")) {
     flash("You don't have permission to access this page");
     die(header("Location: login.php"));
 }
 $id = $_GET["id"];
+$name = $_GET["name"];
+$description = $_GET["description"];
+$category = $_GET["category"];
+$unit_price = $_GET["unit_price"];
+$stock = $_GET["stock"];
+$visibility = $_GET["visibility"];
+
 ?>
 <div>
     <form onsubmit="return validate(this)" method="POST">
         <div class="mb-3">
             <label class="form-label" for="name">Id</label>
-            <input class="form-control" id="id" name="id" value=<?php safer_echo($id)?> />
+            <input class="form-control" id="id" name="id" required value=<?php safer_echo($id) ?> />
         </div>
         <div class="mb-3">
-            <label class="form-label" for="d">Description</label>
-            <textarea class="form-control" name="description" id="d" required ></textarea>
+            <label class="form-label" for="name">Name</label>
+            <input class="form-control" id="name" name="name" required value=<?php safer_echo($name) ?> />
         </div>
         <div class="mb-3">
-            <label class="form-label" for="category">Category</label>
-            <select class="form-control" id="category" name="category">
-                <option value="Other">Other</option>
-                <option value="Food">Food</option>
-                <option value="Clothing">Clothing</option>
-                <option value="Electronics">Electronics</option>
-                <option value="Furniture">Furniture</option>
-                <option value="Toys">Toys</option>
-                <option value="Books">Books</option>
-                <option value="Sports">Sports</option>
-                <option value="Tools">Tools</option>
-                <option value="Games">Games</option>
-                <option value="Movies">Movies</option>
-                <option value="Music">Music</option>
-                <option value="Home">Home</option>
+            <label class="form-label " for="d">Description</label>
+            <textarea class="form-control" name="description" id="d" required ><?php safer_echo($description) ?></textarea>
+        </div>
+        <div class="mb-3">
+            <label class="form-label " for="category">Category</label>
+            <select class="form-control" id="category" name="category" >
+                <option value="Other" <?php echo ($category == "Other" ? "selected" : ""); ?>>Other</option>
+                <option value="Food" <?php echo ($category == "Food" ? "selected" : ""); ?>>Food</option>
+                <option value="Clothing" <?php echo ($category == "Clothing" ? "selected" : ""); ?>>Clothing</option>
+                <option value="Electronics" <?php echo ($category == "Electronics" ? "selected" : ""); ?>>Electronics</option>
+                <option value="Furniture" <?php echo ($category == "Furniture" ? "selected" : ""); ?>>Furniture</option>
+                <option value="Toys" <?php echo ($category == "Toys" ? "selected" : ""); ?>>Toys</option>
+                <option value="Books" <?php echo ($category == "Books" ? "selected" : ""); ?>>Books</option>
+                <option value="Sports" <?php echo ($category == "Sports" ? "selected" : ""); ?>>Sports</option>
+                <option value="Tools" <?php echo ($category == "Tools" ? "selected" : ""); ?>>Tools</option>
+                <option value="Games" <?php echo ($category == "Games" ? "selected" : ""); ?>>Games</option>
+                <option value="Movies" <?php echo ($category == "Movies" ? "selected" : ""); ?>>Movies</option>
+                <option value="Music" <?php echo ($category == "Music" ? "selected" : ""); ?>>Music</option>
+                <option value="Home" <?php echo ($category == "Home" ? "selected" : ""); ?>>Pets</option>
             </select>
         </div>
+
         <div class="mb-3">
             <label class="form-label" for="unit_price">unit_price</label>
-            <input class="form-control" type="number" step="any" id="unit_price" name="unit_price" required />
+            <input class="form-control" type="number" step="any" id="unit_price" name="unit_price" required value=<?php safer_echo($unit_price)?> />
         </div>
         <div class="mb-3">
-            <label class="form-label" for="stock">Stock</label>
-            <input class="form-control" type="number" id="stock" name="stock" required />
-        </div>
-        <div class="mb-3">
-            <label class="form-label" for="visibility">Visibility</label>
-            <input type="checkbox" id="visibility" name="visibility" />
+            <label class="form-label " for="stock">stock</label>
+            <input class="form-control" type="number" id="stock" name="stock" required value=<?php safer_echo($stock)?> />
         </div>
 
-        <input type="Save" class="mt-3 btn btn-primary" value="Submit" />
+        <div class="mb-3">
+            <label class="form-label " for="visibility">visibility</label>
+            <select class="form-control" id="visibility" name="visibility" >
+                <option value="1" <?php echo ($visibility == "1" ? "selected" : ""); ?>>Visible</option>
+                <option value="0" <?php echo ($visibility == "0" ? "selected" : ""); ?>>Hidden</option>
+            </select>
+        </div>
+        <input type="Save" class="mt-3 btn btn-primary" value="Save" />
     </form>
 </div>
 <script>
@@ -56,4 +71,3 @@ $id = $_GET["id"];
         return true;
     }
 </script>
-
--- public_html/Project/admin/list_products.php
+++ public_html/Project/admin/list_products.php
@@ -1,6 +1,6 @@
-<?php 
+<?php
 require __DIR__ . "/../../../partials/nav.php";
-if(!has_role("Admin") && !has_role("Shop Owner")){
+if (!has_role("Admin") && !has_role("Shop Owner")) {
     flash("You don't have permission to access this page");
     die(header("Location: login.php"));
 }
@@ -13,31 +13,39 @@ $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
 <h1>Products</h1>
 <div class="row">
-        <?php foreach ($results as $r): ?>
-            <div class="col-4">
-                <div class="card" style="width: 18rem;">
-                    <div class="card-body">
-                        <h5 class="card-title"><?php safer_echo($r["name"]); ?></h5>
-                        <p class="card-text"><?php safer_echo($r["description"]); ?></p>
-                        <p class="card-text">Category: <?php safer_echo($r["category"]); ?></p>
-                        <p class="card-text">$<?php safer_echo($r["unit_price"]); ?></p>
-                        <p class="card-text">Stock: <?php safer_echo($r["stock"]); ?></p>
-                        <p class="card-text">Visibility: <?php safer_echo($r["visibility"]); ?></p>
-                    </div>
-                    <div class="card-footer">
-                        <a href="ViewCart.php?id=<?php safer_echo($r["id"]); ?>" class="btn btn-primary">Add to Cart</a>
-                    </div>
-                    <?php if(has_role("Admin")): ?>
-                        <div class="card-footer">
-                            <!-- pass in all of current products details to edit_products.php-->
-                            <a href="edit_products.php?id=<?php safer_echo($r['id']); ?>" class="btn btn-primary">Edit</a>
-                        </div>
-                    <?php endif; ?>
+    <?php foreach ($results as $r) : ?>
+        <div class="col-4">
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title"><?php safer_echo($r["name"]); ?></h5>
+                    <p class="card-text"><?php safer_echo($r["description"]); ?></p>
+                    <p class="card-text">Category: <?php safer_echo($r["category"]); ?></p>
+                    <p class="card-text">$<?php safer_echo($r["unit_price"]); ?></p>
+                    <p class="card-text">Stock: <?php safer_echo($r["stock"]); ?></p>
+                    <p class="card-text">Visibility: <?php safer_echo($r["visibility"]); ?></p>
                 </div>
+                <div class="card-footer">
+                    <a href="ViewCart.php?id=<?php safer_echo($r["id"]); ?>" class="btn btn-primary">Add to Cart</a>
+                </div>
+                <?php if (has_role("Admin")) : ?>
+                    <div class="card-footer">
+                        <!-- pass in all of current products details to edit_products.php-->
+                        <a href="edit_products.php?
+                        id=<?php safer_echo($r['id']); ?>&
+                        name=<?php safer_echo($r['name'])?>&
+                        description=<?php safer_echo($r['description'])?>&
+                        category=<?php safer_echo($r['category'])?>&
+                        unit_price=<?php safer_echo($r['unit_price'])?>&
+                        stock=<?php safer_echo($r['stock'])?>&
+                        visibility=<?php safer_echo($r['visibility'])?>"
+                        class="btn btn-primary">Edit</a>
+                    </div>
+                <?php endif; ?>
             </div>
-        <?php endforeach; ?>
-    </div>
+        </div>
+    <?php endforeach; ?>
+</div>
 </div>
 <?php
-require(__DIR__."/../../../partials/flash.php");
+require(__DIR__ . "/../../../partials/flash.php");
 ?>
