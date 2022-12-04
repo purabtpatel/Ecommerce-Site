418af0e15677d6908669be41890091958c8a8a28
purabtpatel <purabtpatel24@gmail.com>
2022-12-04 14:48:02 -0500

    Update ProductDetailsPage.php

--- public_html/Project/ProductDetailsPage.php
+++ public_html/Project/ProductDetailsPage.php
@@ -16,4 +16,18 @@ $stock = $_GET["stock"];
         <p class="card-text">$<?php safer_echo($unit_price); ?></p>
         <p class="card-text">Stock: <?php safer_echo($stock); ?></p>
     </div>
+    <!-- if admin is logged in, show edit button -->
+    <?php if (has_role("Admin")) : ?>
+        <div class="card-footer">
+            <a href="edit_products.php?
+                id=<?php safer_echo($r['id']); ?>&
+                name=<?php safer_echo($r['name']) ?>&
+                description=<?php safer_echo($r['description']) ?>&
+                category=<?php safer_echo($r['category']) ?>&
+                unit_price=<?php safer_echo($r['unit_price']) ?>&
+                stock=<?php safer_echo($r['stock']) ?>&
+                visibility=<?php safer_echo($r['visibility']) ?>" class="btn btn-primary">Edit</a>
+        </div>
+    <?php endif; ?>
+    
 </div>
