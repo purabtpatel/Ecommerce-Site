6f26f88a0c2316c06c032629bb7cc7a5941b4631
purabtpatel <purabtpatel24@gmail.com>
2022-12-09 21:45:52 -0500

    Update ViewCart.php

--- public_html/Project/ViewCart.php
+++ public_html/Project/ViewCart.php
@@ -54,7 +54,7 @@ require __DIR__ . "/../../partials/nav.php";
             <a type="button" class="btn btn-primary" href="checkout.php">Checkout</a>
         </div>
         <div class="col">
-            <button type="button" class="btn btn-primary" onclick="<?php clearcart() ?>">Clear Cart</button>
+            <button type="button" class="btn btn-primary" onclick=<?php clearcart()?>>Clear Cart</button>
         </div>
     </div>
     <?php
