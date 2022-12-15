a4e3e61e57951f0c4fbc3a3ca67daa1512792baf
purabtpatel <purabtpatel24@gmail.com>
2022-12-15 17:14:12 -0500

    Update clear_cart.php

--- public_html/Project/clear_cart.php
+++ public_html/Project/clear_cart.php
@@ -1,4 +1,5 @@
 <?php
+require(__DIR__ . "/../../partials/nav.php");
 $db = getDB();
 $stmt = $db->prepare("DELETE FROM Cart WHERE user_id = :user_id");
 $r = $stmt->execute([":user_id" => get_user_id()]);
@@ -8,4 +9,8 @@ if ($r) {
     flash("Error clearing cart");
 }
 die(header("Location: ViewCart.php"));
+
+?>
+<?php
+require(__DIR__ . "/../../partials/flash.php");
 ?>
